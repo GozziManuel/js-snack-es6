80555eb7963b81a5b1ef3ae9a348d1551ded69bb
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-16 18:45:39 +0100

    array bici + ciclo

---
+++ bici/main.js
@@ -0,0 +1,10 @@
+const bici = [
+  { nome: "monociclo", peso: 150 },
+  { nome: "triciclo", peso: 200 },
+  { nome: "mountainBIKE", peso: 300 },
+  { nome: "bmwX", peso: 420 },
+];
+console.log(bici);
+for (let i = 0; i < bici.length; i++) {
+  let pesoIndex = bici[i].peso;
+}
