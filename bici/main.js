dc2e0fce951802337a779f1a621d0c1518c95b06
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-17 12:44:49 +0100

    added for and push

--- bici/main.js
+++ bici/main.js
@@ -2,9 +2,23 @@ const bici = [
   { nome: "monociclo", peso: 150 },
   { nome: "triciclo", peso: 200 },
   { nome: "mountainBIKE", peso: 300 },
-  { nome: "bmwX", peso: 420 },
+  { nome: "bmwX", peso: 320 },
 ];
-console.log(bici);
+const pesoBici = [];
 for (let i = 0; i < bici.length; i++) {
-  let pesoIndex = bici[i].peso;
+  const index = bici[i].peso;
+  pesoBici.push(index);
 }
+console.log(pesoBici);
+
+function findingSmallNum(array) {
+  let primoNum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < primoNum) {
+      primoNum = array[i];
+    }
+  }
+  return primoNum;
+}
+const minNum = findingSmallNum(pesoBici);
+console.log(minNum);
